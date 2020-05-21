9dfe3990fe4ed091b62703ecbe444fd2d4a66e34
Nick Taylor <nickstaylor@gmail.com>
2020-05-21 10:26:12 -0600

    complete styling and display for iteration 1

--- src/Reservation/Reservation.js
+++ src/Reservation/Reservation.js
@@ -3,13 +3,24 @@ import "./Reservation.css"
 
 const Reservation = (props) => {
   console.log('reservation', props)
+
+  // name = {reservation.name}
+  // date = {reservation.date}
+  // number = {reservation.number}
+  // time = {reservation.time}
+  // id = {reservation.id}
+  // key = {reservation.id}
+  // removeReservation={removeReservation} />
   // <h3>Title: {title}</h3>
   // <p>Description: {description}</p>
   // <button onClick={()=>{deleteIdea(id)}}>🗑</button>
   return (
     <div className="reservation-card">
-    I'm a revervation!
-
+    <h3>{props.name}</h3>
+    <p>{props.date}</p>
+    <p>{props.time}</p>
+    <p>Number of Guests: {props.number}</p>
+    <button className="res-button" onClick={()=>{props.removeReservation(props.id)}}>Cancel</button>
     </div>
   )
 }
