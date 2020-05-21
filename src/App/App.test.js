b711b1ef76f63ee47e8ef38976a52b86749b1a4f
Nick Taylor <nickstaylor@gmail.com>
2020-05-21 11:50:15 -0600

    unit and integration tests for App, Reservations, and Form

--- src/App/App.test.js
+++ src/App/App.test.js
@@ -1,9 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import { fireEvent, render } from "@testing-library/react";
+import "@testing-library/jest-dom/extend-expect";
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
-});
+// Write an integration test for App that
+// tests the user flow for adding a new reservation.
+describe("App", ()=>{
+
+  it('should be able to fill out and submit the reservation form, and add a res to the page', () => {
+    const { getByText, getByRole, getByPlaceholderText } = render(<App />);
+
+    fireEvent.change(getByPlaceholderText('name'), {target: {value: 'Nick'}})
+    fireEvent.change(getByPlaceholderText('Time'), {target: {value: '7pm'}})
+    fireEvent.click(getByText('Make Reservation'));
+
+    expect(getByText('Nick')).toBeInTheDocument()
+    expect(getByText('7pm')).toBeInTheDocument()
+
+  });
+
+
+})
