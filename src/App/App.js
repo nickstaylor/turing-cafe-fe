479e699b571d2444e5bc5e94d640cc1651fc61aa
Nick Taylor <nickstaylor@gmail.com>
2020-05-21 10:14:04 -0600

    iteration 1 set up of fetch call, form display, and initial reservation display

--- src/App/App.js
+++ src/App/App.js
@@ -1,16 +1,50 @@
 import React, { Component } from 'react';
+import Form from '../Form/Form';
+import Reservations from '../Reservations/Reservations'
 import './App.css';
+import { fetchCall } from '../apiCalls/apiCalls'
 
 class App extends Component {
+  constructor(){
+    super()
+    this.state = {
+      reservations: []
+    }
+  }
+
+
+  componentDidMount(){
+    fetchCall()
+    .then(data => {
+      this.setState({reservations: data})
+    })
+    .catch(err => console.log(err))
+  }
+
+  addReservation = () => {
+
+  }
+
+  removeReservation = () => {
+
+  }
+
+
   render() {
+    console.log('reservation', this.state.reservations)
     return (
       <div className="App">
         <h1 className='app-title'>Turing Cafe Reservations</h1>
         <div className='resy-form'>
-
+        <Form addReservation={this.addReservation} />
         </div>
         <div className='resy-container'>
-          
+        
+        <Reservations
+        reservations={this.state.reservations}
+        removeReservation={this.removeReservation}
+        />
+
         </div>
       </div>
     )
