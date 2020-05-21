2315728a0145249dc90962925a5ba31fab4a4fed
Nick Taylor <nickstaylor@gmail.com>
2020-05-21 11:50:41 -0600

    unit and integration tests for App, Reservations, and Form

--- src/index.js
+++ src/index.js
@@ -4,6 +4,5 @@ import './index.css';
 import App from './App/App';
 
 ReactDOM.render(
-  <React.StrictMode>
       <App />
-    </React.StrictMode>, document.getElementById('root'));
+  , document.getElementById('root'));
