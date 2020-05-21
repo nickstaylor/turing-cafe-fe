394867cd72b80b6153d970e8396b3f551b215963
Nick Taylor <nickstaylor@gmail.com>
2020-05-21 10:14:20 -0600

    iteration 1 set up of fetch call, form display, and initial reservation display

---
+++ src/Reservation/Reservation.js
@@ -0,0 +1,16 @@
+import React from "react";
+import "./Reservation.css"
+
+const Reservation = (props) => {
+  console.log('reservation', props)
+  // <h3>Title: {title}</h3>
+  // <p>Description: {description}</p>
+  // <button onClick={()=>{deleteIdea(id)}}>🗑</button>
+  return (
+    <div className="reservation-card">
+    I'm a revervation!
+
+    </div>
+  )
+}
+export default Reservation
