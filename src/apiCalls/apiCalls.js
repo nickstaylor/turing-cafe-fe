fac67a7b8898bb8f84108364ccabf3b088007e1a
Nick Taylor <nickstaylor@gmail.com>
2020-05-21 12:00:17 -0600

    attempted POST, ran out of time

--- src/apiCalls/apiCalls.js
+++ src/apiCalls/apiCalls.js
@@ -4,3 +4,21 @@ export const fetchCall = () => {
 .then(response => response.json())
 
 }
+
+export const postToApi = (newReservation)=>{
+  fetch("http://localhost:3001/api/v1/reservations", {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({
+      id: newReservation.id,
+      name: newReservation.name,
+      date: newReservation.date,
+      time: newReservation.time,
+      number: newReservation.number
+    })
+  })
+  .then(response => response.json())
+  .catch(err => console.log("error", err.message))
+}
