fa217b2aaf424ac547f022680a4dc05d79224536
Nick Taylor <nickstaylor@gmail.com>
2020-05-21 10:14:27 -0600

    iteration 1 set up of fetch call, form display, and initial reservation display

---
+++ src/Reservations/Reservations.js
@@ -0,0 +1,29 @@
+import React from "react";
+import Reservation from "../Reservation/Reservation"
+import "./Reservations.css"
+
+const Reservations = ({reservations, removeReservation}) => {
+  console.log(reservations)
+  const resCards = reservations.map(reservation=>{
+    return (
+      <Reservation
+      name = {reservation.name}
+      date = {reservation.date}
+      number = {reservation.number}
+      time = {reservation.time}
+      id = {reservation.id}
+      key = {reservation.id}
+      removeReservation={removeReservation} />
+    )
+  })
+
+return (
+  <div className="resy-container">
+  reservations here!
+  {resCards}
+  </div>
+)
+
+}
+
+export default Reservations
